a81b94fa4799028cf740811d7b2da1ba172fa8ce
mac Fumiya <fumipumisecky@gmail.com>
2016-02-22 12:48:54 +0900

    add image to Commodity

--- app/src/main/java/com/example/fumiyaseki/yodozon/Commodity.java
+++ app/src/main/java/com/example/fumiyaseki/yodozon/Commodity.java
@@ -1,6 +1,8 @@
 package com.example.fumiyaseki.yodozon;
 
 
+import android.media.Image;
+
 /**
  * Created by fumiyaseki on 2016/02/21.
  */
@@ -8,10 +10,12 @@ public class Commodity {
     private int price;
     private String name;
     private String url;
+    private Image image;
 
-    Commodity(int price, String name, String url){
+    Commodity(int price, String name, String url, Image image){
         this.price = price;
         this.name = name;
         this.url = url;
+        this.image = image;
     }
 }
